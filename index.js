c302ae716f2660864e1d83b57328cc7a65da3d1a
Fawaz Ibraheem <fawazming@gmail.com>
2022-07-28 07:06:36 +0100

    index_upd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-	res.send("Minixpress Reached")
+	res.send("Minixpress Reached and ready to go!")
 })
 
 const port = process.env.PORT || 3000
